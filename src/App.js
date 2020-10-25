be5be9e9ffcf57c06c5ce479544a35d39521cfbd
Pete VS <pete_vs88@me.com>
2020-10-25 09:46:50 -0400

    got basic card up with data passed into

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import Axios from 'axios';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+import Character from './components/Character'
 
 const App = () => {
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+
+  const [characters, setCharacters] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people")
+    .then(({data})=> {
+      const dataResults = data.results
+      setCharacters(dataResults)
+      console.log(data.results)})
+    },[]);
+
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div>
+            {characters.map((character) => {
+                return <Character key={character.name} name={character.name} height={character.height} mass={character.mass} />
+            })}
+            </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,38 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components'
+
+const Character = props => {
+
+    const {name, height, mass} = props;
+
+
+    return (
+        <CardBox>
+            <p>{name}</p>
+        </CardBox>
+    
+    );
+}
+
+const CardBox = styled.div`
+    border-radius: 6px;
+    background: #fff;
+    display: grid;
+    justify-content: stretch;
+    grid-template-columns: 1fr;
+    grid-row-gap: 0;
+    gap: 1rem;
+    margin-top: 1rem;
+    padding: 2rem;
+    box-shadow: 2rem 2rem 6rem rgba(0, 0, 0, 0.15);
+    font-size: 1.6rem;
+    justify-items: center;
+`
+
+const Button = styled.a`
+font-size: 1rem;
+text-transform: uppercase;
+`
+
+export default Character;
